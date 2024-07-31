9a69598aa2d5827e9c8e1b40143e89a1d2e9fcfb
Guilherme Silveira Emanuelli <gui.emanuelli@hotmail.com>
2024-07-31 13:45:47 -0300

    Exercicio 2 Modulo 4

--- Modulo 4/exercicio2 _Filtro(desafio)/helpers.c
+++ Modulo 4/exercicio2 _Filtro(desafio)/helpers.c
@@ -1,8 +1,15 @@
 #include "helpers.h"
 #include <math.h>
+#include <stdio.h>
 
+void edgesDefineTempMatrix(int heightTempMatrix, int widthTempMatrix,
+                           RGBTRIPLE tempMatrix[heightTempMatrix][widthTempMatrix], int height,
+                           int width, RGBTRIPLE image[height][width]);
 RGBTRIPLE setBlurPixel(int startI, int endI, int startJ, int endJ, float totalPixelSum, int height,
                        int width, RGBTRIPLE image[height][width]);
+RGBTRIPLE setEdgePixel(int startHeight, int startWidth, int kernelX[3][3], int kernelY[3][3],
+                       int heightTempMatrix, int widthTempMatrix,
+                       RGBTRIPLE tempMatrix[heightTempMatrix][widthTempMatrix]);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -17,51 +24,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = colorRGBAverage;
             image[i][j].rgbtGreen = colorRGBAverage;
             image[i][j].rgbtBlue = colorRGBAverage;
-        }
-    }
-    return;
-}
-
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            int sepiaRed = round((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) +
-                                 (.189 * image[i][j].rgbtBlue));
-            int sepiaGreen = round((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) +
-                                   (.168 * image[i][j].rgbtBlue));
-            int sepiaBlue = round((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) +
-                                  (.131 * image[i][j].rgbtBlue));
-
-            if (sepiaRed > 255)
-            {
-                image[i][j].rgbtRed = 255;
-            }
-            else
-            {
-                image[i][j].rgbtRed = sepiaRed;
-            }
-
-            if (sepiaGreen > 255)
-            {
-                image[i][j].rgbtGreen = 255;
-            }
-            else
-            {
-                image[i][j].rgbtGreen = sepiaGreen;
-            }
-
-            if (sepiaBlue > 255)
-            {
-                image[i][j].rgbtBlue = 255;
-            }
-            else
-            {
-                image[i][j].rgbtBlue = sepiaBlue;
-            }
         }
     }
     return;
@@ -87,18 +49,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][j] = tempMatrix[i][j];
         }
     }
-
-    // Primeiro codigo que por algum motivo que eu nao sei nao funciona:
-    // RGBTRIPLE tempPixel;
-    // for (int i = 0; i < height; i++)
-    // {
-    //     for (int j = 0, k = width - 1; j < width; j++, k--)
-    //     {
-    //         tempPixel = image[i][k];
-    //         image[i][k] = image[i][j];
-    //         image[i][j] = tempPixel;
-    //     }
-    // }
 }
 
 // Blur image
@@ -167,6 +117,70 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
 }
 
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    int heightTempMatrix = height + 2;
+    int widthTempMatrix = width + 2;
+    RGBTRIPLE tempMatrix[heightTempMatrix][widthTempMatrix];
+
+    edgesDefineTempMatrix(heightTempMatrix, widthTempMatrix, tempMatrix, height, width, image);
+
+    int kernelX[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int kernelY[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] =
+                setEdgePixel(i, j, kernelX, kernelY, heightTempMatrix, widthTempMatrix, tempMatrix);
+        }
+    }
+}
+
+void edgesDefineTempMatrix(int heightTempMatrix, int widthTempMatrix,
+                           RGBTRIPLE tempMatrix[heightTempMatrix][widthTempMatrix], int height,
+                           int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE blackPixel;
+    blackPixel.rgbtRed = 0;
+    blackPixel.rgbtGreen = 0;
+    blackPixel.rgbtBlue = 0;
+
+    for (int i = 0; i < heightTempMatrix; i++)
+    {
+        if (i == 0)
+        {
+            for (int j = 0; j < widthTempMatrix; j++)
+            {
+                tempMatrix[i][j] = blackPixel;
+            }
+        }
+        else if (i == heightTempMatrix - 1)
+        {
+            for (int j = 0; j < widthTempMatrix; j++)
+            {
+                tempMatrix[i][j] = blackPixel;
+            }
+        }
+        else
+        {
+            tempMatrix[i][0] = blackPixel;
+
+            tempMatrix[i][widthTempMatrix - 1] = blackPixel;
+        }
+    }
+
+    for (int i = 1; i < heightTempMatrix - 1; i++)
+    {
+        for (int j = 1; j < widthTempMatrix - 1; j++)
+        {
+            tempMatrix[i][j] = image[i - 1][j - 1];
+        }
+    }
+}
+
 RGBTRIPLE setBlurPixel(int startI, int endI, int startJ, int endJ, float totalPixelSum, int height,
                        int width, RGBTRIPLE image[height][width])
 {
@@ -192,5 +206,62 @@ RGBTRIPLE setBlurPixel(int startI, int endI, int startJ, int endJ, float totalPi
     tempPixel.rgbtRed = round(tempPixelRed / totalPixelSum);
     tempPixel.rgbtGreen = round(tempPixelGreen / totalPixelSum);
     tempPixel.rgbtBlue = round(tempPixelBlue / totalPixelSum);
+    return tempPixel;
+}
+
+RGBTRIPLE setEdgePixel(int startHeight, int startWidth, int kernelX[3][3], int kernelY[3][3],
+                       int heightTempMatrix, int widthTempMatrix,
+                       RGBTRIPLE tempMatrix[heightTempMatrix][widthTempMatrix])
+{
+    RGBTRIPLE tempPixel;
+    int redGx = 0;
+    int greenGx = 0;
+    int blueGx = 0;
+    int redGy = 0;
+    int greenGy = 0;
+    int blueGy = 0;
+
+    for (int i = startHeight, ikernel = 0; i < startHeight + 3; i++, ikernel++)
+    {
+        for (int j = startWidth, jKernel = 0; j < startWidth + 3; j++, jKernel++)
+        {
+            redGx += tempMatrix[i][j].rgbtRed * kernelX[ikernel][jKernel];
+            greenGx += tempMatrix[i][j].rgbtGreen * kernelX[ikernel][jKernel];
+            blueGx += tempMatrix[i][j].rgbtBlue * kernelX[ikernel][jKernel];
+            redGy += tempMatrix[i][j].rgbtRed * kernelY[ikernel][jKernel];
+            greenGy += tempMatrix[i][j].rgbtGreen * kernelY[ikernel][jKernel];
+            blueGy += tempMatrix[i][j].rgbtBlue * kernelY[ikernel][jKernel];
+        }
+    }
+
+    int sumGxGyRed = round(sqrt((double) (redGx * redGx) + (redGy * redGy)));
+    int sumGxGyGreen = round(sqrt((double) (greenGx * greenGx) + (greenGy * greenGy)));
+    int sumGxGyBlue = round(sqrt((double) (blueGx * blueGx) + (blueGy * blueGy)));
+
+    if (sumGxGyRed > 255)
+    {
+        tempPixel.rgbtRed = 255;
+    }
+    else
+    {
+        tempPixel.rgbtRed = sumGxGyRed;
+    }
+    if (sumGxGyGreen > 255)
+    {
+        tempPixel.rgbtGreen = 255;
+    }
+    else
+    {
+        tempPixel.rgbtGreen = sumGxGyGreen;
+    }
+    if (sumGxGyBlue > 255)
+    {
+        tempPixel.rgbtBlue = 255;
+    }
+    else
+    {
+        tempPixel.rgbtBlue = sumGxGyBlue;
+    }
+
     return tempPixel;
 }
